082857df9156b9bc582d4eb94bf831672b2ad415
Jonas Poli <jonas@wab.com.br>
2026-08-31 10:04:07 -0300

    feat: add support for historical data synchronization via start-date, end-date, and year range options

--- src/Command/SyncMedwareCommand.php
+++ src/Command/SyncMedwareCommand.php
@@ -28,7 +28,10 @@ class SyncMedwareCommand extends Command
             ->addOption('loop', 'l', InputOption::VALUE_NONE, 'Executa a sincronização em loop contínuo')
             ->addOption('delay', 'd', InputOption::VALUE_OPTIONAL, 'Intervalo em segundos entre sincronizações no modo loop', 5)
             ->addOption('date', null, InputOption::VALUE_OPTIONAL, 'Data específica para sincronização (formato Y-m-d ou d/m/Y)')
-            ->addOption('days', 'D', InputOption::VALUE_OPTIONAL, 'Quantidade de dias anteriores para sincronizar em lote histórico', 1);
+            ->addOption('days', 'D', InputOption::VALUE_OPTIONAL, 'Quantidade de dias anteriores para sincronizar em lote histórico', 1)
+            ->addOption('start-date', null, InputOption::VALUE_OPTIONAL, 'Data inicial do período histórico (Y-m-d ou d/m/Y)')
+            ->addOption('end-date', null, InputOption::VALUE_OPTIONAL, 'Data final do período histórico (Y-m-d ou d/m/Y)')
+            ->addOption('years', 'Y', InputOption::VALUE_OPTIONAL, 'Anos para trás a importar (ex: 20 para os últimos 20 anos)', null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -46,6 +49,56 @@ class SyncMedwareCommand extends Command
                 $io->error('Formato de data inválido. Use Y-m-d ou d/m/Y.');
                 return Command::FAILURE;
             }
+        }
+
+        $years = $input->getOption('years');
+        $startDateStr = $input->getOption('start-date');
+        $endDateStr = $input->getOption('end-date');
+
+        // Se especificou anos ou intervalo de datas
+        if ($years || $startDateStr) {
+            $dtFim = $endDateStr ? (\DateTime::createFromFormat('d/m/Y', $endDateStr) ?: \DateTime::createFromFormat('Y-m-d', $endDateStr)) : new \DateTime();
+            if ($startDateStr) {
+                $dtInicio = \DateTime::createFromFormat('d/m/Y', $startDateStr) ?: \DateTime::createFromFormat('Y-m-d', $startDateStr);
+            } else {
+                $numYears = (int) $years;
+                $dtInicio = (clone $dtFim)->modify("-{$numYears} years");
+            }
+
+            if (!$dtInicio || !$dtFim) {
+                $io->error('Datas inválidas especificadas.');
+                return Command::FAILURE;
+            }
+
+            $io->title("Iniciando Carga Histórica da API Medware de {$dtInicio->format('d/m/Y')} até {$dtFim->format('d/m/Y')}...");
+
+            $cursor = clone $dtInicio;
+            $totalGeral = 0;
+            $novosGeral = 0;
+            $atualizadosGeral = 0;
+            $diasCount = 0;
+
+            while ($cursor <= $dtFim) {
+                $diaFormatado = $cursor->format('d/m/Y');
+                $io->write("Importando {$diaFormatado}... ");
+
+                $res = $this->medwareClient->sincronizarAgendamentosHoje($cursor);
+
+                if (isset($res['erro'])) {
+                    $io->writeln("<comment>{$res['erro']}</comment>");
+                } else {
+                    $totalGeral += $res['total'];
+                    $novosGeral += $res['novos'];
+                    $atualizadosGeral += $res['atualizados'];
+                    $io->writeln("<info>OK ({$res['total']} registros)</info>");
+                }
+
+                $diasCount++;
+                $cursor->modify('+1 day');
+            }
+
+            $io->success("Carga Histórica Concluída! {$diasCount} dia(s) processado(s). Total: {$totalGeral} (Novos: {$novosGeral}, Atualizados: {$atualizadosGeral})");
+            return Command::SUCCESS;
         }
 
         if ($days > 1) {
